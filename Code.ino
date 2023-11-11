a54236d08d6fb576bc4cc7a7ee400c54862ed10f
Luan Nijou <126830016+Luan-Nijou@users.noreply.github.com>
2023-11-11 18:44:27 -0300

    Update Code.ino

--- Code.ino
+++ Code.ino
@@ -9,20 +9,30 @@
 #define SS_PIN 0
 #define RST_PIN 5
 
-char  ipJson[100];
+char  idJson[100];
+char  PermJson[100];
+char  PagJson[100];
 
-String strID ="AAAAAAAAA";
+float conta = 10;
+float pagar = 10;
 
-MFRC522 rfid(SS_PIN, RST_PIN);
+float valorPagar;
+float valorPerm;
+
+String strID ="AAAAAAAAC";
+String strID2 ="AAAAAAAAC";
+
+
+//MFRC522 rfid(SS_PIN, RST_PIN);
 
 //MQTT and WiFi configuration
 EspMQTTClient client
 (
-  "***",                //nome da sua rede Wi-Fi
-  "****",           //senha da sua rede Wi-Fi
+  "Wokwi-GUEST",                //nome da sua rede Wi-Fi
+  "",           //senha da sua rede Wi-Fi
   "mqtt.tago.io",       //Endereço do servidor MQTT
   "Default",            //User é sempre default pois vamos usar token
-  "****",              // Código do Token
+  "ccdf5e6d-107a-4ee8-932f-c5902a972d1d",              // Código do Token
   "esp32",              //Nome do device
   1883                  //Porta de comunicação padrao
 );
@@ -31,8 +41,8 @@ void setup()
 {
   
    Serial.begin(9600);
-  SPI.begin();
-  rfid.PCD_Init();
+  //SPI.begin();
+ // rfid.PCD_Init();
   
   Serial.println("Conectando WiFi");
   while(!client.isWifiConnected()){
@@ -59,6 +69,7 @@ void onConnectionEstablished()
 //loop do programa
 void loop()
 {
+  
    if (!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
     
   } else {
@@ -71,20 +82,46 @@ void loop()
     }
    }
    
+
     strID.toUpperCase();
     Serial.print("Identificador (UID) da tag: ");
     Serial.println(strID);
     
-if 
+  if(strID == strID2 ){
+    valorPerm = 1;
+  }else{
+    valorPerm = 0;
+  }
+
+  if(conta>=pagar){
+    valorPagar = 1;
+  }else{
+    valorPagar = 0;
+  }
+
+
 
   StaticJsonDocument<300> documentoJson;
   documentoJson["variable"] = "id";
   documentoJson["value"] = strID;
-  serializeJson(documentoJson, ipJson);
+  serializeJson(documentoJson, idJson);
+
+  documentoJson["variable"] = "permissao";
+  documentoJson["value"] = valorPerm;
+  serializeJson(documentoJson, PermJson);
+
+  documentoJson["variable"] = "pagamento";
+  documentoJson["value"] = valorPagar;
+  serializeJson(documentoJson, PagJson);
+
   Serial.println("Enviando os dados");
-  Serial.println(ipJson);
+  Serial.println(idJson);
+  Serial.println(PermJson);
+  Serial.println(PagJson);
   
-  client.publish("topicIP", ipJson); 
+  client.publish("topicIP", idJson); 
+  client.publish("topicIP", PermJson); 
+  client.publish("topicIP", PagJson); 
   delay(5000);
 
   client.loop();
